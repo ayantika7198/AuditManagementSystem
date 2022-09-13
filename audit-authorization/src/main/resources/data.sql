2a61e8c69352db0d3c5797eeb5f6cb29f05fe762
Ayantika <ayantikadatta1608@gmail.com>
2022-09-13 11:58:29 +0530

    First Commit

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Ayantika Datta', 'ayantika', 'ayantika', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Chandni Datta', 'chandni', 'chandni', 'Project-2');
